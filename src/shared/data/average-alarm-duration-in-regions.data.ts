c67593186aec128e3631391b877c31586367a1b8
yurkenchik <yura.ilchshyn@gmail.com>
2025-04-27 00:47:24 +0300

    Added avarage alarm duration a day for each region

---
+++ src/shared/data/average-alarm-duration-in-regions.data.ts
@@ -0,0 +1,22 @@
+
+export const AverageAlarmDurationInRegions: Record<string, number> = {
+    lviv: 0.8,
+    kyiv: 1.04,
+    kharkiv: 9.2,
+    mykolaiv: 0.87,
+    sumy: 1.0,
+    odesa: 1.0,
+    dnipro: 1.23,
+    donetsk: 3.3,
+    chernihiv: 1.0,
+    zaporizhzhia: 1.23,
+    vinnytsia: 1.0,
+    zhytomyr: 0.7,
+    kherson: 0.5,
+    khmelnytskyi: 1.0,
+    ternopil: 1.0,
+    ivanoFrankivsk: 0.87,
+    rivne: 1.0,
+    volyn: 1.13,
+    zakarpattia: 0.7
+} as const;
