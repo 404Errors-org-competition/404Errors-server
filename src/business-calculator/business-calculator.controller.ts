0242b8753e5cc79afc326d69bddd88e65898a8a3
yurkenchik <yura.ilchshyn@gmail.com>
2025-04-27 04:00:15 +0300

    Added proper swagger documentation for encounter controller

--- src/business-calculator/business-calculator.controller.ts
+++ src/business-calculator/business-calculator.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, HttpStatus, Post } from '@nestjs/common';
 import { BusinessCalculatorService } from "src/business-calculator/business-calculator.service";
 import {
     CalculateBusinessProfitRequestDto
@@ -6,11 +6,18 @@ import {
 import {
     CalculateBusinessProfitResponseDto
 } from "src/business-calculator/dto/response/calculate-business-profit-response.dto";
+import { ApiBody, ApiOperation, ApiResponse } from "@nestjs/swagger";
 
 @Controller('business-calculator')
 export class BusinessCalculatorController {
     constructor(private readonly businessCalculatorService: BusinessCalculatorService) {}
 
+    @ApiOperation({
+        summary: '' +
+            'Counting business profit and losses due to air alerts in different regions '
+    })
+    @ApiBody({ type: CalculateBusinessProfitRequestDto })
+    @ApiResponse({ type: CalculateBusinessProfitResponseDto, status: HttpStatus.CREATED })
     @Post()
     async calculateBusinessProfit(
         @Body() calculateBusinessProfitRequestDto: CalculateBusinessProfitRequestDto
